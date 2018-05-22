ec04330ad2e46244a419bf8728b804e37581f00e
loïc delaunay <loicdelaunay05@gmail.com>
2018-05-22 18:40:28 +0200

    Update : procédure stockés

--- ProcédureStockesMission2.sql
+++ ProcédureStockesMission2.sql
@@ -1,10 +1,18 @@
-CREATE OR REPLACE PROCEDURE insert_atelier(plibelle varchar, pnbplacemaxi number)
+CREATE OR REPLACE PROCEDURE insert_atelier(plibelle VARCHAR2, pnbplacemaxi NUMBER)
 is
 erreur exception;
 BEGIN
-INSERT INTO ATELIER (LIBELLEATELIER,NBPLACESMAXI) VALUES (plibelle,pnbplacemaxi);
+INSERT INTO ATELIER (ID,LIBELLEATELIER,NBPLACESMAXI) VALUES (seqatelier.nextval,plibelle,pnbplacemaxi);
 commit;
 exception
     when others then 
         RAISE_APPLICATION_ERROR(-20001, 'Insertion atelier non valide');
 END;
+
+EXEC insert_atelier('test',10);
+
+INSERT INTO ATELIER (ID,LIBELLEATELIER,NBPLACESMAXI) VALUES (seqatelier.nextval,'test',110);
+
+SELECT * FROM ATELIER;
+
+DELETE ATELIER WHERE ID = 10;
